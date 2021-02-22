639c1bd6e3c2efc3e72098d2a9755be7cc925db9
vitorrafael <vrsilveira07@gmail.com>
2021-02-22 18:29:52 -0300

    Adds initial route

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3333;
 
+app.get("/", (req, res) => {
+    return res.json({ message: "Hello World - NLW04!" });
+});
+
 app.listen(PORT, () => {
     console.log("Server is running!");
     console.log(`Listening on port ${PORT}.`);
