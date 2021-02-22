ecd2386a0fe97939c78efa3e744d9a8701b73e1d
vitorrafael <vrsilveira07@gmail.com>
2021-02-22 18:24:17 -0300

    Adds server entrypoint

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3333;
+
+app.listen(PORT, () => {
+    console.log("Server is running!");
+    console.log(`Listening on port ${PORT}.`);
+});
