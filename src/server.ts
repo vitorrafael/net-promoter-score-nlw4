b31e8f345a5c32dda74013c0f8cd7baaf335cb55
vitorrafael <vrsilveira07@gmail.com>
2021-02-22 18:43:15 -0300

    Adds POST route

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
     return res.json({ message: "Hello World - NLW04!" });
 });
 
+app.post("/", (req, res) => {
+    return res.json({ message: "The data was received sucessfully." });
+});
+
 app.listen(PORT, () => {
     console.log("Server is running!");
     console.log(`Listening on port ${PORT}.`);
