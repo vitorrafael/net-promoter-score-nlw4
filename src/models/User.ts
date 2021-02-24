d66e697926d3165615db1d71ba8fb2682c864e32
vitorrafael <vrsilveira07@gmail.com>
2021-02-23 21:57:15 -0300

    Creates User model

---
+++ src/models/User.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+export class User {
+
+    @PrimaryColumn()
+    readonly id: String;
+
+    @Column()
+    name: String;
+
+    @Column()
+    email: String;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
