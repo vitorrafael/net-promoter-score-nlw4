06e85adbaeeffbf907e6e21bb412093c11359826
vitorrafael <vrsilveira07@gmail.com>
2021-02-25 20:40:05 -0300

    Adds SurveyUser model

---
+++ src/models/SurveyUser.ts
@@ -0,0 +1,27 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("surveys_table")
+export default class SurveyUser {
+
+    @PrimaryColumn()
+    readonly id: String;
+
+    @Column()
+    user_id: String;
+
+    @Column()
+    survey_id: String;
+
+    @Column()
+    value: number;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
