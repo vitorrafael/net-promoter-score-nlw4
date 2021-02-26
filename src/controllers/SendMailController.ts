25ba909f5aa47244f52b5e44291c1fdd0160d261
vitorrafael <vrsilveira07@gmail.com>
2021-02-25 22:13:18 -0300

    Refactors SendMailController

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,52 +3,40 @@ import { getCustomRepository } from "typeorm";
 import SurveyRepository from "../repositories/SurveyRepository";
 import SurveyUserRepository from "../repositories/SurveyUserRepository";
 import UserRepository from "../repositories/UserRepository";
+import SendMailService from "../services/SendMailService";
 
 export default class SendMailController {
 
     async execute(request: Request, response: Response) {
         const { email, survey_id } = request.body;
 
-
-        if (!(await SendMailController.userExistsByEmail(email))) {
-            return response.status(400).json({
-                error: "User does not exist."
-            })
-        }
-
-        if(!(await SendMailController.surveyExistsById(survey_id))) {
-            return response.status(400).json({
-                error: "Survey does not exist."
-            })
-        }
-
-
         const userRepository = getCustomRepository(UserRepository);
+        const surveyRepository = getCustomRepository(SurveyRepository);
         const surveyUserRepository = getCustomRepository(SurveyUserRepository);
 
-        const user =  await userRepository.findOne({ email });
+        const user = await userRepository.findOne({ email });
+        if (!user) {
+            return response.status(400).json({
+                error: "User does not exist."
+            });
+        }
+
+        const survey = await surveyRepository.findOne({ id: survey_id });
+        if (!survey) {
+            return response.status(400).json({
+                error: "Survey does not exist."
+            });
+        }
 
         const surveyUser = surveyUserRepository.create({
-            user_id: user && user.id,
+            user_id: user.id,
             survey_id: survey_id
         });
 
         surveyUserRepository.save(surveyUser);
 
+        await SendMailService.execute(email, survey.title, survey.description);
+
         return response.json(surveyUser);
-    }
-
-    static async userExistsByEmail(email: String) {
-        const userRepository = getCustomRepository(UserRepository);
-        const user = await userRepository.findOne({ email });
-
-        return Boolean(user);
-    }
-
-    static async surveyExistsById(id: String) {
-        const surveyRepository = getCustomRepository(SurveyRepository);
-        const survey = await surveyRepository.findOne({ id });
-
-        return Boolean(survey);
     }
 }
