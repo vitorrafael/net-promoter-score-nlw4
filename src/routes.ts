37f397a826a28760413ef8c3999f69a35fe9393f
vitorrafael <vrsilveira07@gmail.com>
2021-02-27 13:56:40 -0300

    Adds Mail Answer endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import UserController from "./controllers/UserController";
 import SurveyController from "./controllers/SurveyController";
 import SendMailController from "./controllers/SendMailController";
+import MailAnswerController from "./controllers/MailAnswerController";
 
 const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
+const mailAnswerController = new MailAnswerController();
 
 router.post("/users", userController.create);
 
@@ -15,5 +17,7 @@ router.get("/surveys", surveyController.show);
 router.post("/surveys", surveyController.create);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", mailAnswerController.execute);
 
 export { router };
