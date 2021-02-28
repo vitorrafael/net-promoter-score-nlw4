15a17e5043ceadeb4dd0ec721cd4906de9cb4609
vitorrafael <vrsilveira07@gmail.com>
2021-02-28 13:01:41 -0300

    Refactors UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,40 +1,38 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
 
 import UserRepository from "../repositories/UserRepository";
 import UserSchema from "../schemas/UserSchema";
 
+import User from "../models/User";
+import ErrorHandler from "../errors/ErrorHandler";
+import Errors from "../enums/Errors";
+
 export default class UserController {
 
+    private userRepository : Repository<User>
+
+    constructor() {
+        this.userRepository = getCustomRepository(UserRepository);
+    }
+
     async create(request: Request, response: Response) {
+
         const { name, email } = request.body;
 
-        if (!UserSchema.isValidSync(request.body)) {
-            return response.status(400).json({
-                error: "Invalid values!"
-            })
-        }
+        ErrorHandler.validate(UserSchema.isValidSync({ name, email }), Errors.INVALID_VALUE);
+        ErrorHandler.validate(! (await this.validateUserAlreadyExistsByEmail(email)), Errors.USER_ALREADY_EXIST);
 
-        const userRepository = getCustomRepository(UserRepository);
+        const user = await this.userRepository.create({name, email});
 
-        const userAlreadyExists = await UserController.validateUserAlreadyExistsByEmail(email);
-
-        if (userAlreadyExists) {
-            return response.status(400).json({
-                error: "User already exists!"
-            })
-        }
-        const user = await userRepository.create({name, email});
-        
-        await userRepository.save(user);
+        await this.userRepository.save(user);
 
         return response.status(201).json(user);
     }
 
-    static async validateUserAlreadyExistsByEmail(email: string) {
-        const userRepository = getCustomRepository(UserRepository);
-        const user = await userRepository.findOne({ email });
-        return Boolean(user);
+    async validateUserAlreadyExistsByEmail(email: string) {
+        const usersWithEmail = await this.userRepository.count({ email });
+        return usersWithEmail === 0;
     }
 
 }
