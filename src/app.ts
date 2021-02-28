7a5facec6ef78e3d1d3e81910071d3ac05359660
vitorrafael <vrsilveira07@gmail.com>
2021-02-28 13:09:16 -0300

    Adds middleware for error handling

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,28 @@
 import "reflect-metadata";
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors";
 import createConnection from "./database";
+import AppError from "./errors/AppError";
 
 const app = express();
 
 createConnection().then(async () => {
     app.use(express.json());
-    
+
     const { router } = await import("./routes");
     app.use(router);
+
+    app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                message: err.message
+            });
+        }
+
+        return response.status(500).json({
+            message: "Internal Server Error"
+        });
+    });
 });
 
 export default app;
